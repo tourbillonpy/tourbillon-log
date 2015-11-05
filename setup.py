6dcc799eceed23711b33ff5ca73086a42fc8a580
Francesco Faraone <ffaraone@gaa9.com>
2015-11-05 17:19:26 +0100

    fix version number documentation

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='tourbillon-log',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     zip_safe=False,
     install_requires=['watchdog==0.8.3'],
